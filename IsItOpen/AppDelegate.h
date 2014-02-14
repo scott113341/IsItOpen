9f91343e6dc3fe7059c68ddbba30f7c692ef6278
Scott Hardy <scott.the.hardy@gmail.com>
2014-02-14 03:00:52 -0800

    Initial Commit

---
+++ IsItOpen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IsItOpen
+//
+//  Created by Scott Hardy on 2/14/14.
+//  Copyright (c) 2014 Scott Hardy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
